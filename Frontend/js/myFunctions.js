2215c05f39dba40ed5bae72ff553937e926d04c5
wi20b063 <84073745+wi20b063@users.noreply.github.com>
2023-04-23 14:37:05 +0200

    CDNs + Website Aufbaut (WiP)

---
+++ Frontend/js/myFunctions.js
@@ -0,0 +1,5 @@
+(function() {
+    //$("head").load(".Frontend/sites/components/head.html");
+    $("nav").load("./Frontend/sites/components/nav.html");
+    $("footer").load("./Frontend/sites/components/nav.html");
+   });
