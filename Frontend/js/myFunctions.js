40ba33e8d7ed0335054264c525f645e786024b11
wi20b063 <84073745+wi20b063@users.noreply.github.com>
2023-05-08 11:18:38 +0200

    Login - Part 1
    Login Form

--- Frontend/js/myFunctions.js
+++ Frontend/js/myFunctions.js
@@ -1,3 +1,44 @@
+$(document).ready(function () {
+    loadHead();
+    loadFooter();  
+  });
+
+function loadHead() {
+    $.$.ajax({
+        type: "GET",
+        url: "../Frontend/sites/components/head.html",
+        dataType: "html",
+        cache: false,
+        success: function (response) {
+            //$("head").html(response);
+            $("head").load("./Frontend/sites/components/head.html");
+        }
+    });
+    //$("head").load("./Frontend/sites/components/head.html");
+}
+
+function loadFooter() {
+    $.$.ajax({
+        type: "GET",
+        url: "../Frontend/sites/components/footer.html",
+        dataType: "html",
+        cache: false,
+        success: function (response) {
+            //$("footer").html(response);
+            $("footer").load("../Frontend/sites/components/footer.html");
+        }
+    });
+    //$("head").load("./Frontend/sites/components/head.html");
+}
+
+
+
+
+
+
+
+
+
 (function() {
     //$("head").load(".Frontend/sites/components/head.html");
     $("nav").load("./Frontend/sites/components/nav.html");
